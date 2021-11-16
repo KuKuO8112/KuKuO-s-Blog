f1f68d1b7d4a33b1abbdfad12878d3352d0704da
KuKuO <a127007966@gmail.com>
2021-11-17 05:37:12 +0800

    titlestyled修改

--- src/components/Layout/Title/TitleStyle.js
+++ src/components/Layout/Title/TitleStyle.js
@@ -7,9 +7,4 @@ font-size: 100px;
 color: #333333;
 text-decoration: none;
 font-family: fantasy;
-transition-duration: 1s;
-&:hover{
-    text-shadow: 8px 8px 8px #666666;
-    transition-duration: 1s;
-}
 `;
