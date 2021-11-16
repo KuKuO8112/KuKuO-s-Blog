8e467ed024bd220ef5aca6b2e15684ccb5331bf9
KuKuO <a127007966@gmail.com>
2021-11-17 05:32:37 +0800

    圖檔大小修改、navlink修改

--- src/components/Layout/Nav/NavLink/PageLink.js
+++ src/components/Layout/Nav/NavLink/PageLink.js
@@ -6,13 +6,8 @@ text-decoration: none;
 font-size: 40px;
 margin-left: 30px;
 margin-right: 30px;
-transition-duration: 1s;
 color: #333333;
 &:visited{
     color: #333333;
-}
-&:hover{
-    text-shadow: 5px 5px 5px #555555;
-    transition-duration: 1s;
 }
 `;
